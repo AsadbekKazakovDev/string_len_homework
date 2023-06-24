7422058609c2cb5585bffbea68d1f353fb0a23ee
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-24 19:48:42 +0500

    7-masala yechimi

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,23 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    s=""
+    a = len(s1)%2==1
+    b = len(s2)%2==1
+    c = len(s3)%2==1
+    if a and b:
+        return f"[{s1},{s2}]"
+    if a and c:
+        return f"[{s1},{s3}]"
+    if b and c:
+        return f"[{s2},{s3}]"
+    if a:
+        return f"[{s1}]"
+    if b:
+        return f"[{s2}]"
+    if c:
+        return f"[{s3}]"
+    if a and b and c:
+        return f"[{s1},{s2},{s3}]"
+s1,s2,s3 = "Code","Academy","markazi"
+print(main(s1,s2,s3))
