a68aef1e619ac4f180a647a38793d8524dbf67fe
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-24 19:59:15 +0500

    8-masala yechimi

--- string_len08.py
+++ string_len08.py
@@ -8,4 +8,13 @@ def main(s):
     Returns:
         str: answer
     """
-    return
+    s1 = ""
+    if len(s)%2==1:
+        s1=s[len(s)//2]
+    if len(s)%2==0:
+        s1 = s[len(s)//2-1:len(s)//2+1]
+    return s1
+s = "cool"
+print(main(s))
+s = "abcdf"
+print(main(s))
